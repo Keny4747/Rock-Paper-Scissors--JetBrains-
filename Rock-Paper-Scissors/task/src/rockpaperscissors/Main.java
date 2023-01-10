a58f2ed4cad378738180283a8159204240c613ab
Keny Pau Vas <keny_pauccarima@usmp.pe>
2023-01-10 10:43:47 -0500

    ...

--- Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
+++ Rock-Paper-Scissors/task/src/rockpaperscissors/Main.java
@@ -4,13 +4,19 @@ import java.util.Random;
 import java.util.Scanner;
 
 enum Option {
-    ROCK("rock"), PAPER("paper"), SCISSORS("scissors");
+    ROCK("rock"),
+    PAPER("paper"),
+    SCISSORS("scissors"),
+    EXIT("!exit");
     String value;
 
     Option(String value) {
         this.value = value;
     }
 
+    public String getValue() {
+        return value;
+    }
 }
 
 enum Message {
@@ -19,12 +25,25 @@ enum Message {
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String userInput = sc.nextLine();
-        String computer = randomGenerate();
+        String flag ="";
+        do {
+            Scanner sc = new Scanner(System.in);
+            String userInput = sc.nextLine();
+            String computer = randomGenerate();
 
-        resultGame(userInput, computer);
+            if(validateInput(userInput)){
+                resultGame(userInput,computer);
 
+            }else if(userInput.equals(Option.EXIT.value)){
+                break;
+            }else{
+                System.out.println("Invalid input");
+            }
+
+            flag = userInput;
+
+        }while(!flag.equals(Option.EXIT.value));
+        System.out.println("Bye!");
     }
 
     public static String randomGenerate() {
@@ -39,22 +58,31 @@ public class Main {
                 return Option.SCISSORS.value;
         }
     }
+    public static boolean validateInput(String userInput){
+
+        switch(userInput){
+            case "paper":
+            case "rock":
+            case "scissors":return true;
+            default: return false;
+        }
+    }
 
     public static void printResultMessage(Message message, String computer) {
         switch (message) {
             case LOSE:
-                System.out.printf("Sorry, but the computer chose %s", computer);
+                System.out.printf("Sorry, but the computer chose %s%n", computer);
                 break;
             case WIN:
-                System.out.printf("Well done. The computer chose %s and failed", computer);
+                System.out.printf("Well done. The computer chose %s and failed%n", computer);
                 break;
             case DRAW:
-                System.out.printf("There is a draw %s", computer);
+                System.out.printf("There is a draw %s%n", computer);
         }
-
     }
 
     public static void resultGame(String user, String computer){
+
         if (user.equals(computer)) {
             printResultMessage(Message.DRAW, computer);
         } else if (user.equals("paper") && computer.equals("rock")) {
@@ -67,7 +95,6 @@ public class Main {
             printResultMessage(Message.LOSE, computer);
         }
     }
-    public static void loop(){
-        
-    }
+
+
 }
